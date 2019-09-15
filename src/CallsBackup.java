6aa17f73d0b51004fb0b315d303cb2442f2efe2f
CodeCrazy97 <evaughan72@yahoo.com>
2019-09-14 20:14:03 -0400

    Eliminated unused imports

--- src/CallsBackup.java
+++ src/CallsBackup.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.*;
 import java.util.LinkedList;
--- src/TextMessagesBackup.java
+++ src/TextMessagesBackup.java
@@ -4,12 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.*;
 import java.util.LinkedList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 class TextMessagesBackup {
 
