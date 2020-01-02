f2a30aa2019838f4f90bf20f164a410b23c589dd
CodeCrazy97 <evaughan72@yahoo.com>
2020-01-02 18:52:38 -0500

    PHP text messages info

---
+++ display_number_of_received_messages_per_contact.php
@@ -0,0 +1,41 @@
+<?php
+
+	function start_table($border=1, $cellspacing=2, $cellpadding=2) {
+		echo "<table border=$border cellspacing=$cellspacing cellpadding=$cellpadding>\n";
+	}
+	
+	function end_table() {
+		$output = "</table>";
+		return $output;
+	}
+	
+	function create_ul($array) {
+		while (list( , $value) = each ($array)) {
+			echo "<tr><td>$value</td></tr>\n";
+		}
+	}
+
+	$mysqli = new mysqli("localhost", "root", "", "phone_backup");
+	
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit;
+	}
+
+	$query = "SELECT c.person_name AS 'person_name', COUNT(*) AS 'cnt'
+		FROM text_messages t 
+		JOIN (SELECT person_name, phone_number FROM contacts) c ON c.phone_number = t.sender_phone_number 
+		WHERE c.person_name != 'Me'
+		GROUP BY t.sender_phone_number
+		ORDER BY COUNT(*) DESC";
+	if ($result = $mysqli->query($query)) {
+		start_table(4,2,5);
+		while ($row = $result->fetch_assoc()) {
+			create_ul($row);
+		}		
+		end_table();
+		$result->free();
+	}
+	$mysqli->close();
+
+?>
